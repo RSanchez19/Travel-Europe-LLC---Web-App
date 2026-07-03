1827714e90cced878d4e7671d363e440e2cd4d7b
RSanchez19 <00191018@uca.edu.sv>
2026-07-02 22:45:57 -0600

    added index.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "<h1>Welcome to Travel Europe LLC<h1>"
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
